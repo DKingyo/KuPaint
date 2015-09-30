ce6ffb032ef45487808a64e02a1617651f367d59
cedricmenet <ced.menet@gmail.com>
2015-10-01 00:49:22 +0200

    some modif

---
+++ WebService/Js/my.js
@@ -0,0 +1,11 @@
+
+
+$( document ).ready( function (e) {
+   // stops modal from being shown
+   $('#myModal').modal('show');
+   $('#button').hide();
+   $('#mainCanvas').hide();
+ 
+   console.log("BLOP");
+   
+})
